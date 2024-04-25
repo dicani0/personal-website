dd5d35d26a991467b983a16844604a62d19d064e
Dawid Miklas <miklasdawid@gmail.com>
2024-04-25 09:52:36 +0200

    Merge commit '0a00bc568652ad74e8a8f8ebd8ad69fea02d25e9' as 'ghPagesSubtree'

---
+++ ghPagesSubtree/src/components/AboutSection.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container mx-auto px-4 min-h-[1000px] h-screen flex flex-col justify-center">
+    <section class="md:shadow-lg lg:shadow-xl shadow-rose-950 md:shadow-rose-950 lg:shadow-rose-950 py-8 md:rounded-3xl lg:rounded-full overflow-hidden">
+      <div class="md:flex md:items-center justify-center">
+        <div class="md:w-2/3 md:mt-0 md:pl-6 text-gray-700">
+          <h2 class="text-4xl font-bold text-gray-800 text-center">About Me</h2>
+          <p class="mt-6">
+            Hello! I'm a dedicated <strong>Backend Developer</strong> with over three years of experience specializing in <strong>PHP</strong> and <strong>Laravel</strong>. I'm passionate about building robust software solutions and am always eager to learn new technologies. I earned my Bachelor of Engineering from Uniwersytet Kaliski im. Prezydenta Stanisława Wojciechowskiego, where I honed my technical skills and developed a solid foundation in software development.
+          </p>
+          <p class="mt-4">
+            Driven by my curiosity and love for technology, I am constantly expanding my knowledge base and skill set to include more full-stack technologies. My professional journey has been marked by a deep commitment to understanding backend architectures and implementing scalable projects that meet the demands of modern applications.
+          </p>
+          <p class="mt-4">
+            Outside of programming, I enjoy a variety of hobbies that keep me refreshed and energized. I am an avid gamer, enjoy target shooting with air guns, and have a passion for racing go-karts. These activities help me maintain a balanced life and fuel my creativity in professional projects.
+          </p>
+          <p class="mt-4">
+            Looking ahead, my goal is to become a proficient Full-Stack Developer, capable of managing and delivering complete web solutions. I am on a continuous learning path, embracing each challenge as an opportunity to grow and contribute to the evolution of the digital landscape.
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
